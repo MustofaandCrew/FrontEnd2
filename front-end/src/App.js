ae5967ca76d7e9170164ea31b3b0b96540847887
garincsr <garincaesar69@gmail.com>
2022-06-23 17:31:29 +0700

    fix vertical layout navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
