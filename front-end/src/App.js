5daa593220afd684e53cd50a9519cbaf090f5ff5
RizkiB12 <rbagus306@gmail.com>
2022-06-23 15:55:32 +0700

    add catalog in pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Catalog from "./pages/Catalog";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+       <Routes>
+        <Route path="/catalog" element={<Catalog />} />
+       </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ front-end/src/pages/Catalog.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import Card from "../components/Card";
+
+
+import NewProducts from "../assets/images/NewProducts.jpg";
+import NewProducts2 from "../assets/images/NewProducts2.jpg";
+import NewProducts3 from "../assets/images/NewProducts3.jpg";
+import LayoutNavbarFooter from '../components/LayoutNavbarFooter';
+
+export default function Catalog() {
+    return (
+        <LayoutNavbarFooter>
+            <div class="container">
+                <div class="row">
+                    <div class="col-9">
+                        <h2>Catalog</h2>
+                    </div>
+                    <div class="col-4 pt-2">
+                        <br />
+                        <div className="border border-2 border-secondary rounded px-3 py-3">
+                            <h5>Categories</h5>
+                            <br />
+                            <a href="/" className="link"><p>All Categories</p></a>
+                            <hr />
+                            <a href="/" className="link"><p>Wishlist</p></a>
+                            <hr />
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <div className="row row-cols-2 row-cols-md-4 g-4 mt-2 mb-5">
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                            <Card image={NewProducts2} />
+                            <Card image={NewProducts3} />
+                            <Card image={NewProducts} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </LayoutNavbarFooter>
+    )
+}
