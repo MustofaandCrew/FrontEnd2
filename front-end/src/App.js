b6bd27cd7c0939e22c037eca34e15f659447e13c
RizkiB12 <rbagus306@gmail.com>
2022-06-23 16:05:41 +0700

    add wishlist in pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Wishlist from "./pages/Wishlist";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Routes>
+        <Route path="/wishlist" element={<Wishlist/>} />
+      </Routes>
       </BrowserRouter>
     </div>
   );
