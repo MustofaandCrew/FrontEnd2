a2f6c268567388a63c95b96ab6cadea378df5c0d
RizkiB12 <rbagus306@gmail.com>
2022-06-23 19:01:47 +0700

    update app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 
-=======
->>>>>>> e3b433db12cc9f74e56e600a601b7c4660bfabaf
 import Wishlist from "./pages/Wishlist";
 // import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
