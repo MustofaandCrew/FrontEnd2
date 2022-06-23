e3b433db12cc9f74e56e600a601b7c4660bfabaf
garincsr <garincaesar69@gmail.com>
2022-06-23 17:01:42 +0700

    responsive wishlist page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Wishlist from "./pages/Wishlist";
 
 function App() {
