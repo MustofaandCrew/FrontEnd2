a63c1ab0325b0630fea67a5a2ab5defd5129c67d
garincsr <garincaesar69@gmail.com>
2022-06-24 20:53:17 +0700

    add css modal navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
+        <Footer />
       </BrowserRouter>
     </div>
   );
