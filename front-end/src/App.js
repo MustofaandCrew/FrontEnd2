b5ca3aff19d011bb9c14abd4f826bff9e89bae57
RizkiB12 <rbagus306@gmail.com>
2022-06-27 14:49:56 +0700

    add shooping-cart in pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Shoppingcart from "./pages/Shoppingcart";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Footer />
+      <Routes>
+        <Route path="/shoppingcart" element={<Shoppingcart />} />
+      </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ front-end/src/pages/Shoppingcart.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import LayoutNavbarFooter from "../components/LayoutNavbarFooter";
+
+import Shoe from "../assets/images/NewProducts2.jpg";
+import Doll from "../assets/images/NewProducts.jpg";
+import Watch from "../assets/images/NewProducts3.jpg";
+
+
+export default function Shoppingcart() {
+    return (
+        <LayoutNavbarFooter>
+            <div className="mb-5">
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Name & Seller</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Menu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td><img src={Shoe} alt="shoes" width={150} /></td>
+                            <td>
+                                <h4>Otto</h4>
+                                <p>By Andika Suka</p>
+                            </td>
+                            <td>
+                                <h4>$29,112</h4>
+                                <p>USD</p>
+                            </td>
+                            <td><button className="btn btn-danger">Remove</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td><img src={Doll} alt="shoes" width={150} /></td>
+                            <td>
+                                <h4>Otto</h4>
+                                <p>By Andika Suka</p>
+                            </td>
+                            <td>
+                                <h4>$29,112</h4>
+                                <p>USD</p>
+                            </td>
+                            <td><button className="btn btn-danger">Remove</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td><img src={Watch} alt="shoes" width={150} /></td>
+                            <td>
+                                <h4>Otto</h4>
+                                <p>By Andika Suka</p>
+                            </td>
+                            <td>
+                                <h4>$29,112</h4>
+                                <p>USD</p>
+                            </td>
+                            <td><button className="btn btn-danger">Remove</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td><img src={Doll} alt="shoes" width={150} /></td>
+                            <td>
+                                <h4>Otto</h4>
+                                <p>By Andika Suka</p>
+                            </td>
+                            <td>
+                                <h4>$29,112</h4>
+                                <p>USD</p>
+                            </td>
+                            <td><button className="btn btn-danger">Remove</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </LayoutNavbarFooter>
+    )
+}
