26e27a9c943c9a02535d2b3c4b24e43799cf556e
RizkiB12 <rbagus306@gmail.com>
2022-06-27 14:08:58 +0700

    add home in pages and remake it

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Footer />
+       <Routes>
+        <Route path="/" element={<Home/>} />
+       </Routes>
       </BrowserRouter>
     </div>
   );
