1a446a460b9e2c88a69ce977e0bc63077845e179
SatrioBayu <sbayu875@gmail.com>
2022-07-22 20:32:48 +0700

    feat: edit footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -4,29 +4,35 @@ import "../assets/css/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="mt-5">
+    <div className="mt-5 footer-container">
       <hr />
       <div className="container">
         <div className="row g-3 footer-row">
           <div className="col-lg-6">
             <h5 className="text-uppercase">About Us</h5>
             <p>
-            SecondHand is a place to buy and sell goods online, especially "secondhand" goods. This platform opens and provides various types of need categories. Registered users
-                In this application, you can act as a seller and a buyer by using the same 1 (one) account. This platform will bring together sellers and buyers to be able to negotiate
-                goods and make transactions directly outside the platform.
+              SecondHand is a place to buy and sell goods online, especially "secondhand" goods. This platform opens and provides various types of need categories. Registered users In this
+              application, you can act as a seller and a buyer by using the same 1 (one) account. This platform will bring together sellers and buyers to be able to negotiate goods and make
+              transactions directly outside the platform.
             </p>
           </div>
           <div className="col-md-2">
             <h5 className="text-uppercase">Links</h5>
             <ul className="list-unstyled">
               <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink className="footer-link" to="/">
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/login">Login</NavLink>
+                <NavLink className="footer-link" to="/login">
+                  Login
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/register">Register</NavLink>
+                <NavLink className="footer-link" to="/register">
+                  Register
+                </NavLink>
               </li>
             </ul>
           </div>
@@ -36,38 +42,58 @@ export default function Footer() {
               <div className="col-md-6">
                 <ul className="list-unstyled">
                   <li>
-                    <NavLink to="/catalog?category=Electronic">Electronic</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Electronic">
+                      Electronic
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Gadgets">Gadgets</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Gadgets">
+                      Gadgets
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Sneakers">Sneakers</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Sneakers">
+                      Sneakers
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Fashion">Fashion</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Fashion">
+                      Fashion
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Tools">Tools</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Tools">
+                      Tools
+                    </NavLink>
                   </li>
                 </ul>
               </div>
               <div className="col-md-6">
                 <ul className="list-unstyled">
                   <li>
-                    <NavLink to="/catalog?category=Health%20and%20Beauty">Health & Beauty</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Health%20and%20Beauty">
+                      Health & Beauty
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Baby">Baby</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Baby">
+                      Baby
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Hobby">Hobby</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Hobby">
+                      Hobby
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Gaming">Gaming</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Gaming">
+                      Gaming
+                    </NavLink>
                   </li>
                   <li>
-                    <NavLink to="/catalog?category=Furniture">Furniture</NavLink>
+                    <NavLink className="footer-link" to="/catalog?category=Furniture">
+                      Furniture
+                    </NavLink>
                   </li>
                 </ul>
               </div>
